1d1a600fad1ef5102ddc2cbee398dd5c7da389fb
Bereket Bizuayehu Teshome <106858299+Bekabiz@users.noreply.github.com>
2026-07-14 15:05:09 +0300

    fix(ui): restore structural stylesheet beneath redesign layer

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import ErrorBoundary from './components/ErrorBoundary'
+import './App.css'
 import './product-ui.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
